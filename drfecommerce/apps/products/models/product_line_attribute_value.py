44fc5bfc81bceb5324c0e025ff315a08a6a73401
Yuriy Rybak <jura.ryback007@gmail.com>
2024-10-17 18:20:11 +0300

    Added validation for attribute values in product line.

--- drfecommerce/apps/products/models/product_line_attribute_value.py
+++ drfecommerce/apps/products/models/product_line_attribute_value.py
@@ -1,5 +1,7 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
+from drfecommerce.apps.products.models import Attribute
 from drfecommerce.apps.products.models.attribute_value import AttributeValue
 from drfecommerce.apps.products.models.product_line import ProductLine
 
@@ -12,3 +14,22 @@ class ProductLineAttributeValue(models.Model):
         unique_together = ('attribute_value', 'product_line')
         verbose_name = 'Product Line Attribute Value'
         verbose_name_plural = 'Product Line Attribute Values'
+
+    def clean(self):
+        queryset = (
+            ProductLineAttributeValue.objects.filter(attribute_value=self.attribute_value)
+            .filter(product_line=self.product_line)
+            .exists()
+        )
+
+        if not queryset:
+            iqs = Attribute.objects.filter(attribute_values__product_lines=self.product_line).values_list(
+                'pk', flat=True
+            )
+
+            if self.attribute_value.attribute.id in list(iqs):
+                raise ValidationError("Duplicate attribute exists")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(ProductLineAttributeValue, self).save(*args, **kwargs)
--- drfecommerce/apps/products/serializers/product_line.py
+++ drfecommerce/apps/products/serializers/product_line.py
@@ -19,3 +19,9 @@ class ProductLineSerializer(serializers.ModelSerializer):
             'product_images',
             'attribute_values',
         ]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        attribute_values = {item['attribute']['name']: item['value'] for item in data.pop('attribute_values')}
+        data['specifications'] = attribute_values
+        return data
